f505c79e0d4323d602c18b163b418912ea5efa96
Kevin Xu <xuxinzheng24@gmail.com>
2019-12-02 12:06:15 -0500

    Completed feature deleteSongById

--- song-microservice/src/main/java/com/csc301/songmicroservice/SongDalImpl.java
+++ song-microservice/src/main/java/com/csc301/songmicroservice/SongDalImpl.java
@@ -89,8 +89,29 @@ public class SongDalImpl implements SongDal {
 
 	@Override
 	public DbQueryStatus deleteSongById(String songId) {
-		// TODO Auto-generated method stub
-		return null;
+		DbQueryStatus dbQueryStatus = new DbQueryStatus("OK", DbQueryExecResult.QUERY_OK);
+		try {
+			if (StringUtils.isEmpty(songId)) {
+				dbQueryStatus.setMessage("ERROR_GENERIC");
+				dbQueryStatus.setdbQueryExecResult(DbQueryExecResult.QUERY_ERROR_GENERIC);
+				return dbQueryStatus;
+			}
+			Song obj = db.findById(songId, Song.class);
+
+			if (obj == null) {
+				dbQueryStatus.setMessage("NOT_FOUND");
+				dbQueryStatus.setdbQueryExecResult(DbQueryExecResult.QUERY_ERROR_NOT_FOUND);
+				return dbQueryStatus;
+			}
+
+			db.remove(obj);
+//			dbQueryStatus.setData(obj);
+		} catch (RuntimeException ex) {
+			dbQueryStatus.setMessage("ERROR_GENERIC");
+			dbQueryStatus.setdbQueryExecResult(DbQueryExecResult.QUERY_ERROR_GENERIC);
+			ex.printStackTrace();
+		}
+		return dbQueryStatus;
 	}
 
 	@Override
