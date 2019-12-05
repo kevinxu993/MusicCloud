c3a3b438ef8ee6a2481e3d607ebde6e8c18fe311
chengyu66 <1998xinchengyu@gmail.com>
2019-12-05 16:58:12 -0500

    Implemented unlikeSong function

--- profile-microservice/src/main/java/com/csc301/profilemicroservice/ProfileController.java
+++ profile-microservice/src/main/java/com/csc301/profilemicroservice/ProfileController.java
@@ -107,7 +107,10 @@ public class ProfileController {
 		Map<String, Object> response = new HashMap<String, Object>();
 		response.put("path", String.format("PUT %s", Utils.getUrl(request)));
 
-		return null;
+		DbQueryStatus dbQueryStatus = playlistDriver.unlikeSong(userName, songId);
+		response = Utils.setResponseStatus(response, dbQueryStatus.getdbQueryExecResult(), dbQueryStatus.getData());
+
+		return response;
 	}
 
 	@RequestMapping(value = "/deleteAllSongsFromDb/{songId}", method = RequestMethod.PUT)
